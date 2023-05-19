808a0164a3b6d014ca05c38f605efa94379767c1
diogobnfr <diogobonofre@gmail.com>
2023-05-19 00:42:34 -0300

    Added while and do while loops examples

---
+++ while_loop/Program.cs
@@ -0,0 +1,16 @@
+﻿/* string name = "";
+
+while (name == "")
+{
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+} */
+
+string name;
+do
+{
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+} while (name == "");
+
+Console.WriteLine("Hello " + name);

124a16fb43657452297af86c6ce48aa4a795a382
diogobnfr <diogobonofre@gmail.com>
2023-05-19 00:31:56 -0300

    Added switches and logical operators examples

---
+++ logical_operators/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("What's the temperature outside (C): ");
+double temp = Convert.ToDouble(Console.ReadLine());
+
+if (temp >= 10 && temp <= 25)
+{
+    Console.WriteLine("It's warm outside!");
+}
+else if (temp <= -50 || temp >= 50)
+{
+    Console.WriteLine("DO NOT GO OUTSIDE!");
+}

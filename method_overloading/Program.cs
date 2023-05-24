4c5dea3627afb28029c3ba3c18fb06d08045388e
diogobnfr <diogobonofre@gmail.com>
2023-05-23 21:17:49 -0300

    Added method overloading examples

---
+++ method_overloading/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        double total = Multiply(2, 3, 5);
+        Console.WriteLine("a . b . c = " + total);
+        
+    }
+    
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    public static double Multiply(double a, double b, double c) // method signature == name + parameters
+    {
+        return a * b * c;
+    }
+}

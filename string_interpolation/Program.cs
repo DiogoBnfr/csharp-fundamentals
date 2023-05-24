8e13c59ff7fffcd4933ae3d8bd632621dfd3359d
diogobnfr <diogobonofre@gmail.com>
2023-05-24 12:45:11 -0300

    Added string interpolation and multidimensional arrays examples

---
+++ string_interpolation/Program.cs
@@ -0,0 +1,7 @@
+﻿string brand = "mitsubish";
+string line = "lancer";
+string model = "evo";
+
+Console.WriteLine($"{"Brand: " + brand, 30}" +
+                  $"{"Line: " + line, 30}" +
+                  $"{"Model: "+ model, 30}");

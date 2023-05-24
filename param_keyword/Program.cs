1670d56453be1240d66cbe5e8ecd6ee375c3f8b0
diogobnfr <diogobonofre@gmail.com>
2023-05-23 21:33:47 -0300

    Added param keyword examples

---
+++ param_keyword/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Price: " + CheckOut(15.99, 2.27, 122.98, 13.80));
+    }
+
+    public static double CheckOut(params double[] prices)
+    {
+        double total = 0;
+        foreach (double price in prices)
+        {
+            total += price;
+        }
+        return total;
+    }
+}

b4336eecee15322b3d9dc3fcf57b2d2025153bdb
diogobnfr <diogobonofre@gmail.com>
2023-05-23 23:05:11 -0300

    Added conditional operator example

---
+++ conditional_operator/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("What's the temperature outside? ");
+        double temperature = Convert.ToDouble(Console.ReadLine());
+        string diagnostic = (temperature >= 15) ? "It's warm outside!" : "It's cold outside!";
+        Console.WriteLine(diagnostic);
+    }
+}

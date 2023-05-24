ee5e8e939439760627fa1aef577456e9d35756e0
diogobnfr <diogobonofre@gmail.com>
2023-05-24 20:00:12 -0300

    Added objects as args example

---
+++ objects_args/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car1 = new Car("Mustang", "Red");
+        Car car2 = Copy(car1);
+        
+        Console.WriteLine("Your " + car2.Model + " is " + car2.Color + "!");
+    }
+
+    public static Car Copy(Car car)
+    {
+        return new Car(car.Model, car.Color);
+    }
+}
+
+class Car
+{
+    public string Model;
+    public string Color;
+    public static int TotalCars;
+    public Car(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+        TotalCars++;
+    }
+}

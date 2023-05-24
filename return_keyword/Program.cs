0633ce13ca5cbc9d300c5374d2df1984f04f2217
diogobnfr <diogobonofre@gmail.com>
2023-05-23 21:12:10 -0300

    Added return example

---
+++ return_keyword/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double x, y;
+        
+        Console.Write("Enter number 1: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter number 2: ");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("x . y = " + Multiply(x, y));
+    }
+
+    static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+}

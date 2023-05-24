90118fae43ba3e314f20aeeb55bc30da202dbf4f
diogobnfr <diogobonofre@gmail.com>
2023-05-24 18:04:52 -0300

    Added overloaded constructors examples

---
+++ overloaded_constructors/Program.cs
@@ -0,0 +1,9 @@
+﻿using overloaded_constructors;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Pizza pizza = new Pizza("stuffed crust", "red sauce", "prato"); // can have 1, 2, 3 or 4 args because of the multiple constructors
+    }
+}

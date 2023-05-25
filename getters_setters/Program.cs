5327be33056cd7613edea07ec8c7974b618d0efc
diogobnfr <diogobonofre@gmail.com>
2023-05-25 20:56:22 -0300

    Added getters and setters examples

---
+++ getters_setters/Program.cs
@@ -0,0 +1,25 @@
+﻿ internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car(400);
+        car.Speed = 100000;
+        Console.WriteLine(car.Speed);
+    }
+
+    class Car
+    {
+        public int speed;
+
+        public Car(int speed)
+        {
+            Speed = speed;
+        }
+
+        public int Speed
+        {
+            get => speed; // read
+            set => speed = value > 500 ? 500 : value; // write
+        }
+    }
+}

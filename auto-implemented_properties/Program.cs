009654309ddd78cad3c2906fb8cc6e68d42dec08
diogobnfr <diogobonofre@gmail.com>
2023-05-25 21:19:47 -0300

    Added auto-implemented properties example

---
+++ auto-implemented_properties/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car("Porsche");
+        Console.WriteLine(car.Model);
+    }
+
+    class Car
+    {
+        public string Model { get; set; }
+
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+    }
+}
